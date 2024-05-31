e3347d872faf5f38f6c9e8c27f6022268e2404ff
SanteriWille <santeriwille@gmail.com>
2024-05-31 13:22:49 +0200

    Jobbet med teknologiforståelse siden og lagt til flere bilder

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar'); 
-let navLinks = document.querySelectorAll('header nav a');
+let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
   menuIcon.classList.toggle('bx-x');
