9ae7ad1078c564f3623bfb9d7f87b74511440c8d
SanteriWille <santeriwille@gmail.com>
2024-06-06 13:26:22 +0200

    Lagde en kontakt side, lagt til all informasjonen jeg hadde tenkt.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* Hamburger meny */
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -6,3 +7,27 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('active');
 }
+
+/* Kontakt siden */
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  const inputs = form.querySelectorAll(".input-field .item, .textarea-field .item");
+  const errorMessages = form.querySelectorAll(".error-txt");
+
+  form.addEventListener("submit", (event) => {
+    let valid = true;
+
+    inputs.forEach((input, index) => {
+      if (input.value.trim() === "") {
+        errorMessages[index].style.display = "block";
+        valid = false;
+      } else {
+        errorMessages[index].style.display = "none";
+      }
+    });
+
+    if (!valid) {
+      event.preventDefault();
+    }
+  });
+});
